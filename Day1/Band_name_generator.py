0d8c5995ff125286ab2d33b0fc601885e41da305
sejalpuraswani <100908528+sejalpuraswani@users.noreply.github.com>
2022-03-04 18:05:03 +0530

    Add files via upload

---
+++ Day1/Band_name_generator.py
@@ -0,0 +1,10 @@
+#print function will print the statement
+print("Welcome to the Band Name Generator.")
+
+#It will take a input from user and store that in a variable
+city_name = input("What's name of the city you grew up in?\n")
+
+pet_name = input("What's your pet's name?\n")
+
+#it will concatenate two strings and print the final output
+print("Your band name could be " + city_name + " " + pet_name)
