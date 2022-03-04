fcfd9c74fbcfe92d1feffedcc898ffbdd23fb33a
sejalpuraswani <100908528+sejalpuraswani@users.noreply.github.com>
2022-03-04 17:34:42 +0530

    Add files via upload

---
+++ Day1/Swapping_values.py
@@ -0,0 +1,12 @@
+#Taking two inputs from user
+a = input("a: ")
+b = input("b: ")
+
+#Swapping values using temporary variable c
+c = a
+a = b
+b = c
+
+#printing the values of a and b
+print("a: " + a)
+print("b: " + b)
