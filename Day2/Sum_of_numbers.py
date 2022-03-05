708a9725f6cf825e75807ceaa9f690f6f8a74bd5
sejalpuraswani <100908528+sejalpuraswani@users.noreply.github.com>
2022-03-05 22:28:09 +0530

    Add files via upload

---
+++ Day2/Sum_of_numbers.py
@@ -0,0 +1,7 @@
+two_digit_number = input("Type a two digit number: ")
+
+first_num = int(two_digit_number[0])
+second_num = int(two_digit_number[1])
+
+result = first_num + second_num
+print(result)
