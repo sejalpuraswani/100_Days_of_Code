4402f166eef99d9b4e968c0fd144acd37865d7f2
sejalpuraswani <100908528+sejalpuraswani@users.noreply.github.com>
2022-03-06 17:01:43 +0530

    Add files via upload

---
+++ Day4/Nested_list.py
@@ -0,0 +1,7 @@
+fruits = ["Strawberries","Nectarines","Apples","Peaches","Grapes","Pears"]
+
+vegetables = ["Spinach","Kale","Tomatoes","Celery","Potatoes"]
+
+combined_list = [fruits, vegetables]
+
+print(combined_list)
